991146d2328f6b5c1ec822c77c050d051b5e88bd
Adam Blomberg <halikular@icloud.com>
2019-02-02 01:34:16 +0100

    First Commit

---
+++ src/MatchmakingPlugin.java
@@ -0,0 +1,15 @@
+
+public class MatchmakingPlugin {
+
+	public static void main(String[] args) {
+		
+		int player1 = 130;
+		int player2 = 110;
+		
+		if ((player1 - 20) <= player2 && player2 <= (player1 + 20)) {
+			System.out.println("Match together");
+		}
+
+	}
+
+}
